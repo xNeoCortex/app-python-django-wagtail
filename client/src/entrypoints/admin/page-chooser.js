0e1ce7696df35285b57a6621faa62b1030df2c79
Matt Westcott <matthew@torchbox.com>
2021-01-07 15:04:27 +0000

    Revert "Fix page chooser getValue so that it doesn't fail on an empty state"
    This reverts commit 7b8d5f504cdbad444dae595309ae04b07cd583e7.

--- client/src/entrypoints/admin/page-chooser.js
+++ client/src/entrypoints/admin/page-chooser.js
@@ -28,7 +28,7 @@ function createPageChooser(id, openAtParentId, config) {
   /* define public API functions for the chooser */
   const chooser = {
     getState: () => state,
-    getValue: () => state && state.id,
+    getValue: () => state.id,
     setState: (newState) => {
       if (newState) {
         input.val(newState.id);
